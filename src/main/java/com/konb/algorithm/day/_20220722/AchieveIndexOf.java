fdfb722dfd202a97400d8d6b876b99a5e1446d50
konb-hui <2581881495@qq.com>
2022-07-23 09:55:14 +0800

    update day 20220722

--- src/main/java/com/konb/algorithm/day/_20220722/AchieveIndexOf.java
+++ src/main/java/com/konb/algorithm/day/_20220722/AchieveIndexOf.java
@@ -1,5 +1,8 @@
 package com.konb.algorithm.day._20220722;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author konb
  * @version 1.0
@@ -12,42 +15,53 @@ public class AchieveIndexOf {
         int length1 = haystack.length();
         int length2 = needle.length();
 
-        int i = 0, j = 0;
-        int k = i;
+        if (length2 > length1) {
+            return -1;
+        }
+
+        Map<Character, Integer> map = init(needle);
+
+        int i = length2 - 1, j = i;
+        int k = 0;
 
         while (true) {
-            if (haystack.charAt(i ++) == needle.charAt(j ++)) {
-                if (j >= length2) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                if (-- j < 0) {
                     break;
                 }
+                -- i;
             } else {
                 int index = k + length2;
                 if (index >= length1) {
                     return -1;
                 }
-                int moveNum = length2 - indexOf(needle, haystack.charAt(index));
-                i = i + moveNum;
-                k = i;
-                j = 0;
+                Integer temp = map.get(haystack.charAt(index));
+                int moveNum = length2 - (temp == null ? -1 : temp);
+                k = k + moveNum;
+                i = k + length2 - 1;
+                j = length2 - 1;
+
+                if (i >= length1) {
+                    return -1;
+                }
             }
         }
         return k;
     }
 
-    public int indexOf(String needle, char a) {
-        for (int i = needle.length() - 1; i >= 0; i --) {
-            if (a == needle.charAt(i)) {
-                return i;
-            }
+    public Map<Character, Integer> init(String needle) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < needle.length(); i ++) {
+            map.put(needle.charAt(i), i);
         }
 
-        return -1;
+        return map;
     }
 //"mississippi"
 //        "issipi"
     public static void main(String[] args) {
         AchieveIndexOf achieveIndexOf = new AchieveIndexOf();
-        System.out.println(achieveIndexOf.strStr("abc", "c"));
+        System.out.println(achieveIndexOf.strStr("mississipii", "issipi"));
     }
 
 }
