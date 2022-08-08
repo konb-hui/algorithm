11f5a768b3ab7d7f1e386641524de6c99b369f06
konb-hui <2581881495@qq.com>
2022-08-08 18:42:19 +0800

    update day 20220808

--- src/main/java/com/konb/algorithm/day/_2022/_08/_06/SearchRotationSortArray.java
+++ src/main/java/com/konb/algorithm/day/_2022/_08/_06/SearchRotationSortArray.java
@@ -8,50 +8,50 @@ package com.konb.algorithm.day._2022._08._06;
  */
 public class SearchRotationSortArray {
 
-    public int search(int[] nums, int target) {
-        if (nums[0] == target) {
-            return 0;
-        }
+        public int search(int[] nums, int target) {
+            if (nums[0] == target) {
+                return 0;
+            }
 
-        boolean flag = nums[0] > target;
+            boolean flag = nums[0] > target;
 
-        int left = 0;
-        int right = nums.length;
+            int left = 0;
+            int right = nums.length - 1;
 
-        while (left <= right) {
-            int mid = (right + left) >> 1;
-            int a = nums[mid];
-            if (a == target) {
-                return mid;
-            }else if (a < target) {
-                if (flag) {
-                    left = mid;
-                } else {
-                    if (a < nums[0]) {
-                        right = mid;
+            while (left <= right) {
+                int mid = (right + left) >> 1;
+                int a = nums[mid];
+                if (a == target) {
+                    return mid;
+                }else if (a < target) {
+                    if (flag) {
+                        left = mid + 1;
                     } else {
-                        left = mid;
-                    }
-                }
-            } else {
-                if (flag) {
-                    if (a < nums[0]) {
-                        right = mid;
-                    } else {
-                        left = mid;
+                        if (a < nums[0]) {
+                            right = mid - 1;
+                        } else {
+                            left = mid + 1;
+                        }
                     }
                 } else {
-                    left = mid;
+                    if (flag) {
+                        if (a < nums[0]) {
+                            right = mid - 1;
+                        } else {
+                            left = mid + 1;
+                        }
+                    } else {
+                        right = mid - 1;
+                    }
                 }
             }
-        }
 
-        return  -1;
-    }
+            return  -1;
+        }
 
     public static void main(String[] args) {
         SearchRotationSortArray searchRotationSortArray = new SearchRotationSortArray();
-        System.out.println(searchRotationSortArray.search(new int[] {4,5,6,7,0,1,2}, 3));
+        System.out.println(searchRotationSortArray.search(new int[] {4, 5, 6, 7, 0, 1, 2}, 5));
     }
 
 }
